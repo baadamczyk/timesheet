4e401356d23d7049bd54a53124d613b8c6195a19
baadamczyk <bartlomiej.adamczyk92@gmail.com>
2017-04-10 20:40:43 +0200

    initial Spring setup

---
+++ bin/README.md
@@ -0,0 +1,7 @@
+# timesheet
+Java time tracking project (WebApplication readme)
+
+<b>v.0.1.1 TODO:</b>
+- [ ] Spring/Maven project setup
+- [ ] Database model
+- [ ] REST API
---
+++ src/main/java/pl/baadamczyk/TimesheetwebApplication.java
@@ -0,0 +1,12 @@
+package pl.baadamczyk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimesheetwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimesheetwebApplication.class, args);
+	}
+}
